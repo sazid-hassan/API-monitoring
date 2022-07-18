01a1ccebb447af25fe2b3ffd67f24afc968e28cd
sazid-hassan <sazid.khan.hassan@gmail.com>
2022-07-18 10:14:50 +0600

    req formatting

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 const http = require('http');
 
+const url = require('url')
+
 const app = {};
 
 app.config = {
@@ -15,12 +17,29 @@ app.config = {
 };
 
 app.createServer = () => {
-    const server = http.createServer(handleReqRes);
+    const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
         console.log(`Listening to port ${app.config.port}`)
     });
 }
 
 app.handleReqRes = (req, res) => {
+
+    const parsedURL = url.parse(req.url, true);
+
+    const path = parsedURL.pathname;
+
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const method = req.method.toLowerCase();
+
+    const queryString = parsedURL.query;
+
+    const headersObj = req.headers;
+
+    console.log(headersObj);
+
     res.end('some message');
 }
+
+app.createServer();
